6a207d16a943d6fb15ad94cd95b7aaba22b3b1b5
frizzon00 <alexandre.frizon@sptech.school>
2024-11-29 23:13:41 -0300

    Voltando Credenciais AWS

--- src/main/java/aws_connection/S3Provider.java
+++ src/main/java/aws_connection/S3Provider.java
@@ -1,6 +1,6 @@
 package aws_connection;
 
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
@@ -9,11 +9,22 @@ import java.io.InputStream;
 
 public class S3Provider {
 
-    // Cria e Configura um Cliente em S3 usando as credenciais padrões
+    private final AwsSessionCredentials credentials;
+
+    // Busca as Variaveis Ambiente Configuradas Temporariamente no Sistema
+    public S3Provider() {
+        this.credentials = AwsSessionCredentials.create(
+                System.getenv("AWS_ACCESS_KEY_ID"),
+                System.getenv("AWS_SECRET_ACCESS_KEY"),
+                System.getenv("AWS_SESSION_TOKEN")
+        );
+    }
+
+    // Cria e Configura um Cliente em S3 para Buscar o Arquivo com as Credencias Cadastradas
     public S3Client getS3Client() {
         return S3Client.builder()
-                .region(Region.US_EAST_1)  // Região desejada
-                .credentialsProvider(DefaultCredentialsProvider.create())  // Usa as credenciais padrão
+                .region(Region.US_EAST_1)
+                .credentialsProvider(() -> credentials)
                 .build();
     }
 
